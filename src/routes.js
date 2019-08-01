fa2b5e2dca3d0942abd9b0d274ff57f71ad3e8bb
Bruno Barbosa <brunobarbosa96@hotmail.com>
2019-07-31 22:05:18 -0300

    Aula 15 - Adicionar middleware de autenticaçao.

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionControlller";
 
+import authMiddlaware from "./app/middlewares/auth";
+
 const routes = new Router();
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddlaware);
+
+routes.put("/users", UserController.update);
 
 export default routes;
