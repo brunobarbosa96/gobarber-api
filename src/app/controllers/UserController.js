6f4f4303c7166afea7b52049e25ea02b1c881ccf
Bruno Barbosa <brunobarbosa96@hotmail.com>
2019-07-31 22:51:43 -0300

    Aula 16 - Alterando usuario com validaçao de senha..

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,7 +19,34 @@ class UserController {
     }
 
     async update(req, res) {
-        return res.json({ ok: true });
+        const { email, oldPassword } = req.body;
+        const user = await User.findByPk(req.userId);
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email }
+            });
+
+            if (userExists)
+                return res.status(400).json({ error: "User already exists" });
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword)))
+            return res.status(401).json({ error: "Password does not match" });
+
+        await User.update(req.body, {
+            where: { id: req.userId }
+        });
+
+        const { id, name, provider } = await User.findOne({
+            where: { email }
+        });
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider
+        });
     }
 }
 
